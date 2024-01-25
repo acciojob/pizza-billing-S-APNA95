6c600feb9cc51e23138ace731e442eafae1584a9
S-APNA95 <sapna.acharya3@gmail.com>
2024-01-25 17:53:17 +0530

    Pizza Bill App

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,17 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
+
         super(isVeg);
         // your code goes here
+        this.extraCheesePrice = 80;
+        if(isVeg){
+            this.extraToppingPrice = 70;
+
+        }
+        else{
+            this.extraToppingPrice = 120;
+        }
+
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,12 +2,40 @@ package com.driver;
 
 public class Pizza {
 
+
     private int price;
     private Boolean isVeg;
     private String bill;
+    public int extraToppingPrice;
+
+    private int basicExtraToppingPrice;
+    public int extraCheesePrice;
+    private int basicExtraCheesePrice;
+    private int pizzaBasePrice;
+    private int paperBagPrice;
+    private boolean takeAway;
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
+
+        this.basicExtraCheesePrice = 80;
+        this.paperBagPrice = 20;
+        if(isVeg){
+            this.basicExtraToppingPrice = 70;
+            this.pizzaBasePrice = 300;
+            this.price = this.pizzaBasePrice;
+
+        }
+        else{
+            this.basicExtraToppingPrice = 120;
+              this.pizzaBasePrice = 400;
+            this.price = this.pizzaBasePrice;
+        }
+        this.bill = "Base Price Of The Pizza: "+ this.pizzaBasePrice + "\n";
+
+
         // your code goes here
     }
 
@@ -17,18 +45,45 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+
+        this.extraCheesePrice += this.basicExtraCheesePrice;
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+
+        this.extraToppingPrice += this.basicExtraToppingPrice;
+
+
     }
 
     public void addTakeaway(){
         // your code goes here
+        this.takeAway = true;
+
+
+
     }
 
     public String getBill(){
         // your code goes here
+        if(this.extraCheesePrice != 0){
+            this.price += this.extraCheesePrice;
+            this.bill += "Extra Cheese Added: " + this.extraCheesePrice + "\n";
+        }
+
+        if(this.extraToppingPrice != 0) {
+            this.price += this.extraToppingPrice;
+            this.bill += "Extra Toppings Added: " + this.extraToppingPrice + "\n";
+        }
+            if(this.takeAway){
+           this.price += this.paperBagPrice;
+           this.bill += "Paperbag Added: " + this.paperBagPrice +"\n";
+       }
+
+        this.bill += "Total Price: "+ this.price + "\n";
         return this.bill;
+
     }
 }
